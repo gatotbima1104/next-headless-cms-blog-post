1c5a39b5f4925d880ffd280af9d50caf9d7fb5a5
Muhamad Gatot Supiadin <gatotbima2002@gmail.com>
2024-12-14 21:01:41 +0700

    feat: fix urls image

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images : {
+  images: {
     formats: ['image/webp'],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.ctfassets.net"
-      }
-
-    ]
-  }
+        hostname: "images.ctfassets.net",
+      },
+      {
+        protocol: "https",
+        hostname: "*.contentful.com", // For other Contentful domains
+      },
+    ],
+  },
+  
 };
 
 export default nextConfig;
